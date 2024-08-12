1c05664b3198224f3196c47b7d6c33d9c63a202f
RazvanConstantin1 <razvanconstantin1097@gmail.com>
2024-08-12 13:06:43 +0300

    implemented dynamic nav bar

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,33 @@
+import Link from "next/link.js";
+import { Button } from "./ui/button.jsx";
+
+// components
+import Nav from "./Nav.jsx";
+
 function Header() {
-  return <header>header</header>;
+  return (
+    <header className="py-8 xl:py-12 text-white bg-pink-50/20">
+      <div className="container mx-auto flex justify-between items-center">
+        {/* logo */}
+        <Link href="/">
+          <h1 className="text-4xl font-semibold">
+            Razvan <span className="text-accent">.</span>
+          </h1>
+        </Link>
+
+        {/* desktop nav & hire me button */}
+        <div className="hidden xl:flex items-center gap-8">
+          <Nav />
+          <Link href="/contact">
+            <Button>Hire Me</Button>
+          </Link>
+        </div>
+
+        {/* mobile nav */}
+        <div className="xl:hidden">mobile nav</div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
